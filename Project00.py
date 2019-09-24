57848929efa4e01efe7eb20224adf5b7d625cc06
Alex Voultos <voultoa@gmail.com>
2019-09-23 21:55:24 -0400

    Comments added

--- Project00.py
+++ Project00.py
@@ -1,6 +1,10 @@
 
 def test(letters):
-
+    """
+    Main function for Project0 python file
+    :param letters: (Tuple) - Valid strings to be used for character validation
+    :return:
+    """
     with open("words.txt", 'r') as wordFile:
         new_words = wordFile.readlines()
 
@@ -10,8 +14,10 @@ def test(letters):
     # Contains the return list for the printout
     returnList = []
 
+    # If the word in new_words is longer than 7 and the word contains all valid chars, append to returnList
     for word in new_words:
         word = word.replace('\n', '')
+        # Checks if the length of the word is equal to 7
         if len(word) == 7:
             if checker(word, checkerDict):
                 returnList.append(word)
@@ -19,7 +25,14 @@ def test(letters):
 
 
 def checker(word, checkerDict):
-
+    """
+    Checker function for Project0
+    :param word: (String) - String to check for validity
+    :param checkerDict: (Dict) - Dictionary that contains the valid characters and max usage of those chars
+    :return:
+        True if passes validation
+        False if fails validation
+    """
     for letter in word:
         try:
             if word.lower().count(letter) > checkerDict[letter]:
@@ -30,7 +43,11 @@ def checker(word, checkerDict):
 
 
 def combLetters(letters):
-
+    """
+    Function that returns a dictionary of how many characters were used in the letters variable
+    :param letters: (Tuple) - Tuple of strings used as valid letters
+    :return:
+    """
     retDict = {}
     for item in letters:
         for letter in item:
